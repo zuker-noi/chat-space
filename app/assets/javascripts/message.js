effbf5b0280e662d79945a898b90ecfe3d6cfca6
zuker-noi <inoshishi44@gmail.com>
2020-05-30 18:33:50 +0900

    create asynchronous system

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,2 +1,65 @@
-$(function(){
+$(function(){ 
+  function buildHTML(message){
+   if ( message.image ) {
+     var html =
+      `<div class="chat-main__message-list__message">
+         <div class="chat-main__message-list__message__name-box">
+           <div class="chat-main__message-list__message__name-box__name">
+             ${message.user_name}
+           </div>
+           <div class="chat-main__message-list__message__name-box__date">
+             ${message.created_at}
+           </div>
+         </div>
+         <div class="chat-main__message-list__text">
+           <p class="chat-main__message-list__text__content">
+             ${message.content}
+           </p>
+         </div>
+         <img src=${message.image} >
+       </div>`
+     return html;
+   } else {
+     var html =
+      `<div class="chat-main__message-list__message">
+         <div class="chat-main__message-list__message__name-box">
+           <div class="chat-main__message-list__message__name-box__name">
+             ${message.user_name}
+           </div>
+           <div class="chat-main__message-list__message__name-box__date">
+             ${message.created_at}
+           </div>
+         </div>
+         <div class="chat-main__message-list__text">
+           <p class="chat-main__message-list__text__content">
+             ${message.content}
+           </p>
+         </div>
+       </div>`
+     return html;
+   };
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $('.chat-main__message-form__input-box__submit-btn').removeAttr('data-disable-with');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+     .done(function(data){
+      var html = buildHTML(data);
+      $('.chat-main__message-list').append(html);
+      $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+      $('form')[0].reset();
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
+  })
 });
